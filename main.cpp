b84d6e19f194c6a146998a6309e387e62ff94c83
notnuff <113949291+notnuff@users.noreply.github.com>
2025-04-11 15:20:41 +0300

    figured out how to actually use FUSE; beginning of VFS creation;

--- main.cpp
+++ main.cpp
@@ -1,147 +1,53 @@
-/*
-  FUSE: Filesystem in Userspace
-  Copyright (C) 2001-2007  Miklos Szeredi <miklos@szeredi.hu>
-
-  This program can be distributed under the terms of the GNU GPLv2.
-  See the file COPYING.
-*/
-
-/** @file
- *
- * minimal example filesystem using high-level API
- *
- * Compile with:
- *
- *     gcc -Wall hello.c `pkg-config fuse3 --cflags --libs` -o hello
- *
- * ## Source code ##
- * \include hello.c
- */
-
-
 #define FUSE_USE_VERSION 31
 
 #include <fuse.h>
-#include <stdio.h>
-#include <string.h>
-#include <errno.h>
+#include <cstdio>
+#include <cstring>
+#include <cerrno>
 #include <fcntl.h>
-#include <stddef.h>
-#include <assert.h>
+#include <cstddef>
+#include <cassert>
+#include <unistd.h>
 
-/*
- * Command line options
- *
- * We can't set default values for the char* fields here because
- * fuse_opt_parse would attempt to free() them when the user specifies
- * different values on the command line.
- */
-static struct options {
-	const char *filename;
-	const char *contents;
-	int show_help;
-} options;
+class Descriptor {
+public:
+	Descriptor() = default;
+	virtual ~Descriptor() = default;
 
-#define OPTION(t, p)                           \
-    { t, offsetof(struct options, p), 1 }
-static const struct fuse_opt option_spec[] = {
-	OPTION("--name=%s", filename),
-	OPTION("--contents=%s", contents),
-	OPTION("-h", show_help),
-	OPTION("--help", show_help),
+	virtual mode_t Type() = 0;
+
+};
+
+class Directory : public Descriptor {
+public:
+	mode_t Type() override { return S_IFDIR; };
+
+protected:
+
+};
+
+class File : public Descriptor {
+public:
+	mode_t Type() override { return S_IFREG; };
+
+protected:
+
+
+};
+
+
+struct ParsedOptions {
+	bool show_help;
+};
+
+static const fuse_opt availableOptions[] = {
+	{"-h", offsetof(struct ParsedOptions, show_help), true},
+	{"--help", offsetof(struct ParsedOptions, show_help), true},
 	FUSE_OPT_END
 };
 
-static void *hello_init(struct fuse_conn_info *conn,
-			struct fuse_config *cfg)
-{
-	(void) conn;
-	cfg->kernel_cache = 1;
-
-	/* Test setting flags the old way */
-	conn->want = FUSE_CAP_ASYNC_READ;
-	conn->want &= ~FUSE_CAP_ASYNC_READ;
-
-	return NULL;
-}
-
-static int hello_getattr(const char *path, struct stat *stbuf,
-			 struct fuse_file_info *fi)
-{
-	(void) fi;
-	int res = 0;
-
-	memset(stbuf, 0, sizeof(struct stat));
-	if (strcmp(path, "/") == 0) {
-		stbuf->st_mode = S_IFDIR | 0755;
-		stbuf->st_nlink = 2;
-	} else if (strcmp(path+1, options.filename) == 0) {
-		stbuf->st_mode = S_IFREG | 0444;
-		stbuf->st_nlink = 1;
-		stbuf->st_size = strlen(options.contents);
-	} else
-		res = -ENOENT;
-
-	return res;
-}
-
-static int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-			 off_t offset, struct fuse_file_info *fi,
-			 enum fuse_readdir_flags flags)
-{
-	(void) offset;
-	(void) fi;
-	(void) flags;
-
-	if (strcmp(path, "/") != 0)
-		return -ENOENT;
-
-	filler(buf, ".", NULL, 0, FUSE_FILL_DIR_PLUS);
-	filler(buf, "..", NULL, 0, FUSE_FILL_DIR_PLUS);
-	filler(buf, options.filename, NULL, 0, FUSE_FILL_DIR_PLUS);
-
-	return 0;
-}
-
-static int hello_open(const char *path, struct fuse_file_info *fi)
-{
-	if (strcmp(path+1, options.filename) != 0)
-		return -ENOENT;
-
-	if ((fi->flags & O_ACCMODE) != O_RDONLY)
-		return -EACCES;
-
-	return 0;
-}
-
-static int hello_read(const char *path, char *buf, size_t size, off_t offset,
-		      struct fuse_file_info *fi)
-{
-	size_t len;
-	(void) fi;
-	if(strcmp(path+1, options.filename) != 0)
-		return -ENOENT;
-
-	len = strlen(options.contents);
-	if (offset < len) {
-		if (offset + size > len)
-			size = len - offset;
-		memcpy(buf, options.contents + offset, size);
-	} else
-		size = 0;
-
-	return size;
-}
-
-
 static void show_help(const char *progname) {
-	printf("usage: %s [options] <mountpoint>\n\n", progname);
-	printf("File-system specific options:\n"
-	       "    --name=<s>          Name of the \"hello\" file\n"
-	       "                        (default: \"hello\")\n"
-	       "    --contents=<s>      Contents \"hello\" file\n"
-	       "                        (default \"Hello, World!\\n\")\n"
-	       "\n");
+	printf("usage: %s <mountpoint>\n\n", progname);
 }
 
 
@@ -154,45 +60,130 @@ static void show_help(const char *progname) {
 // };
 
 // I'm surprised this wrapper actually worked
-struct fuse_operations_wrapper : public fuse_operations {
-	fuse_operations_wrapper() : fuse_operations() {
-		init	= hello_init;
-		getattr	= hello_getattr;
-		readdir	= hello_readdir;
-		open	= hello_open;
-		read	= hello_read;
-	};
+struct FuseOperationsWrapper : public fuse_operations {
+	FuseOperationsWrapper() : fuse_operations() {
+
+		// fuse_operations::init = init;
+		fuse_operations::getattr = getattr;
+		fuse_operations::readdir = readdir;
+		fuse_operations::open = open;
+		fuse_operations::read = read;
+	}
+
+protected:
+	/**
+	 * Initialize filesystem
+	 *
+	 * The return value will passed in the `private_data` field of
+	 * `struct fuse_context` to all file operations, and as a
+	 * parameter to the destroy() method. It overrides the initial
+	 * value provided to fuse_main() / fuse_new().
+	 */
+	static void* init(fuse_conn_info *conn, fuse_config *cfg) {
+		cfg->kernel_cache = 1;
+
+		return nullptr;
+	}
+
+
+	// stbuf - attributes about a file or directory.
+	static int getattr (const char *path, struct stat *stbuf, fuse_file_info *fi) {
+		// explicit way to suppress compiler warnings about not used value
+		(void) fi;
+
+		// flushing the buffer
+		flush_buffer(stbuf);
+
+		if (strcmp(path, "/") == 0) {
+			stbuf->st_mode = S_IFDIR | 0755;
+
+			// link count is 2 because it counts for / and .
+			stbuf->st_nlink = 2;
+		} else if (strcmp(path, "/test") == 0) {
+			stbuf->st_mode = S_IFREG | 0555;
+			stbuf->st_nlink = 1;
+			stbuf->st_size = 10;
+		} else {
+			return -ENOENT;
+		}
+
+		return 0;
+	}
+
+	static int readdir (const char *path, void *buf, fuse_fill_dir_t filler,
+			 off_t offset, fuse_file_info *fi,
+			 fuse_readdir_flags flags) {
+
+		if (strcmp(path, "/") == 0) {
+			filler(buf, ".", nullptr, 0, FUSE_FILL_DIR_PLUS);
+			filler(buf, "..", nullptr, 0, FUSE_FILL_DIR_PLUS);
+			filler(buf, "test", nullptr, 0, FUSE_FILL_DIR_PLUS);
+		} else {
+			return -ENOENT;
+		}
+
+		return 0;
+	}
+
+	static int open (const char *path, fuse_file_info *fi) {
+		if (strcmp(path, "/test") != 0)
+			return -ENOENT;
+
+		if ((fi->flags & O_ACCMODE) != O_RDONLY)
+			return -EACCES;
+
+		return 0;
+	}
+
+	static int read (const char *path, char *buf, size_t size, off_t offset,
+			  fuse_file_info *fi) {
+		size_t len;
+		if(strcmp(path, "/test") != 0)
+			return -ENOENT;
+
+		const auto content = "hi-hi";
+
+		len = strlen(content);
+		if (offset < len) {
+			if (offset + size > len)
+				size = len - offset;
+			memcpy(buf, content + offset, size);
+		} else
+			size = 0;
+
+		return size;
+	}
+
+protected:
+	template <typename T>
+	static void flush_buffer(T* buffer) {
+		memset(buffer, 0, sizeof(T));
+	}
+
 };
 
-static const struct fuse_operations_wrapper hello_oper{};
+static const FuseOperationsWrapper tinyVFSOperations{};
 
-int main(int argc, char *argv[])
-{
-	int ret;
-	struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
+int main(int argc, char *argv[]) {
+	fuse_args args = FUSE_ARGS_INIT(argc, argv);
 
-	/* Set defaults -- we have to use strdup so that
-	   fuse_opt_parse can free the defaults if other
-	   values are specified */
-	options.filename = strdup("hello");
-	options.contents = strdup("Hello World!\n");
+	ParsedOptions opt{};
+	// parse options - in our case it's -h or --help only
+	if (fuse_opt_parse(&args, &opt, availableOptions, nullptr) == -1) return 1;
 
-	/* Parse options */
-	if (fuse_opt_parse(&args, &options, option_spec, NULL) == -1)
-		return 1;
-
-	/* When --help is specified, first print our own file-system
-	   specific help text, then signal fuse_main to show
-	   additional help (by adding `--help` to the options again)
-	   without usage: line (by setting argv[0] to the empty
-	   string) */
-	if (options.show_help) {
+	// When --help is specified, first print our own file-system
+	// specific help text, then signal fuse_main to show
+	// additional help (by adding `--help` to the options again)
+	// without usage: line (by setting argv[0] to the empty string)
+	if (opt.show_help) {
 		show_help(argv[0]);
 		assert(fuse_opt_add_arg(&args, "--help") == 0);
 		args.argv[0][0] = '\0';
 	}
 
-	ret = fuse_main(args.argc, args.argv, &hello_oper, NULL);
+	// assert(fuse_opt_add_arg(&args, "-odefault_permissions") == 0);
+
+	auto ret = fuse_main(args.argc, args.argv, &tinyVFSOperations, nullptr);
 	fuse_opt_free_args(&args);
 	return ret;
 }
